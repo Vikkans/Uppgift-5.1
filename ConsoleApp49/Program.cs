fab26270a7f107f18fe1d4004711ad8fbcbc9a05
Vikkans <Jakobsenviktor6@gmail.com>
2023-11-28 18:18:32 +0100

    Add project files.

---
+++ ConsoleApp49/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[4];
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("skriv in ett namn");
+                names[i] = Console.ReadLine();
+               
+            }
+            for (int j = 0; j < 5; j++)
+            {
+                Console.WriteLine(names[j]);
+            }
+
+        }
+    }
+}
